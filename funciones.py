782ffb61b8a8ac27e4455a0a4d4b1d19b445ede6
Milanesa21 <diegojara88574@gmail.com>
2024-04-11 12:14:32 -0300

    Finalizacion del trabajo

---
+++ funciones.py
@@ -0,0 +1,58 @@
+import csv as c
+from tarea import connect_db, insert_db, select_db, count_localidades, create_db
+
+
+def super_funcion():
+    def read_csv():
+        with open('localidades.csv', mode='r')as read:
+            reader = c.DictReader(read)
+            db = connect_db()
+            create_db(db)
+            rows_to_insert = []
+            for row in reader:
+                provincia = row['provincia']
+                id = int(row['id'])
+                localidad = row['localidad']
+                cp = int(row['cp']) if row ['cp'] != '' else None
+                id_prov_mstr = int(row['id_prov_mstr'])if row ['id_prov_mstr'] != '' else None
+                rows_to_insert.append((provincia, id, localidad, cp, id_prov_mstr))
+            insert_db(db, rows_to_insert)
+
+
+    def create_csv():
+        db = connect_db()
+        result = select_db(db)
+        count = count_localidades(db)
+        
+        provincias = {}
+        provincia_total = {}
+
+        for row in result:
+            provincia = row[0]
+            if provincia not in provincias:
+                provincias[provincia] = []
+            provincias[provincia].append(row)
+        
+        for row in count:
+            provincia = row[0]
+            total = row[1]
+            provincia_total[provincia] = total
+
+
+        for provincia, localidades in provincias.items():
+            with open(f'{provincia}.csv', mode='w', encoding='utf-8', newline='') as f:
+                fieldnames = ['localidad']
+                writer = c.DictWriter(f, fieldnames=fieldnames)
+                writer.writeheader()
+                for localidad in localidades:
+                    writer.writerow({'localidad': localidad[2]})
+            
+            with open(f'{provincia}.csv', mode='a', encoding='utf-8', newline='') as f:
+                fieldnames = ['Total']
+                writer = c.DictWriter(f, fieldnames=fieldnames)
+                writer.writeheader()
+                for pro_total, total in provincia_total.items():
+                    if provincia == pro_total:
+                        writer.writerow({'Total': total})
+    create_csv()
+    read_csv()
--- tarea.py
+++ tarea.py
@@ -1,6 +1,8 @@
 import sys
 import MySQLdb
 
+
+
 def connect_db():
     try:
         db = MySQLdb.connect("localhost","root","","test")
@@ -8,7 +10,13 @@ def connect_db():
         print("No se pudo conectar:", e)
         sys.exit(1)
     print("Conexion correcta")
+    return db
+
+def create_db(db):
     cursor = db.cursor()
+    sql_delete = "DROP TABLE IF EXISTS localidades"
+    cursor.execute(sql_delete)
+    db.commit()
     sql_create = """CREATE TABLE IF NOT EXISTS localidades(
     provincia VARCHAR(255),
     id INT,
@@ -16,7 +24,6 @@ def connect_db():
     cp INT,
     id_prov_mstr INT)"""
     cursor.execute(sql_create)
-    return db
 
 
 def insert_db(db, rows_to_insert):
@@ -26,5 +33,19 @@ def insert_db(db, rows_to_insert):
     cursor.executemany(sql_insert, rows_to_insert)
     db.commit()
 
+
+def select_db(db):
+    cursor = db.cursor()
+    sql_select = "SELECT * FROM localidades"
+    cursor.execute(sql_select)
+    result = cursor.fetchall()
+    return result
+
+def count_localidades(db):
+    cursor = db.cursor()
+    sql_count = "SELECT provincia, COUNT(*) FROM localidades GROUP BY provincia"
+    cursor.execute(sql_count)
+    result = cursor.fetchall()
+    return result
 
 
